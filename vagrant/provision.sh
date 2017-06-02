077f8ed55aef1a2431554d303ba54f8560c05c61
Maciej Jagiellowicz <maciej.jagiellowicz@allegrogroup.com>
2017-06-02 09:26:59 +0200

    fix virtualenv setup

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,20 +1,24 @@
 
 # Install essential packages
-apt-get update
-apt-get upgrade
-apt-get -y install mc vim git 2> /dev/null
-apt-get -y install redis-server 2> /dev/null
-apt-get -y install build-essential libbz2-dev libfreetype6-dev libgdbm-dev python3-dev 2> /dev/null
-apt-get -y install python3-pip 2> /dev/null
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install mc vim git 2> /dev/null
+sudo apt-get -y install redis-server 2> /dev/null
+sudo apt-get -y install build-essential libbz2-dev libfreetype6-dev libgdbm-dev python3-dev 2> /dev/null
+sudo apt-get -y install python3-pip 2> /dev/null
 
 # Create virtualenv for python 3.4
-pip3 install virtualenvwrapper
+sudo pip3 install virtualenvwrapper
 VIRTUALENVS='/home/ubuntu/.virtualenvs'
 if [ ! -d $VIRTUALENVS ]; then
 mkdir /home/ubuntu/.virtualenvs
+export WORKON_HOME=$VIRTUALENVS
+export VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv alamo
 fi
-chown -R ubuntu:ubuntu $VIRTUALENVS
-sudo -u ubuntu bash -c "export WORKON_HOME=$VIRTUALENVS"
+
+
 if [ `grep alamo /home/ubuntu/.bashrc | wc -l` = 0 ]; then
 echo "VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'" >> /home/ubuntu/.bashrc
 echo 'command' >> /home/ubuntu/.bashrc
@@ -25,8 +29,11 @@ echo 'workon alamo' >> /home/ubuntu/.bashrc
 echo 'else' >> /home/ubuntu/.bashrc
 echo 'mkvirtualenv alamo' >> /home/ubuntu/.bashrc
 echo 'fi' >> /home/ubuntu/.bashrc
+echo 'export ALAMO_SCHEDULERS=`hostname -f`' >> /home/ubuntu/.bashrc
 fi
 
 # Install application environment
-su -l ubuntu -c '/home/ubuntu/.virtualenvs/alamo/bin/pip install ipython tox'
-su -l ubuntu -c '/home/ubuntu/.virtualenvs/alamo/bin/pip install asyncio asyncio_redis aioconsul aioamqp apscheduler'
+/home/ubuntu/.virtualenvs/alamo/bin/pip install ipython tox
+/home/ubuntu/.virtualenvs/alamo/bin/pip install asyncio asyncio_redis aioconsul aioamqp apscheduler
+/home/ubuntu/.virtualenvs/alamo/bin/pip install -r /home/ubuntu/project/requirements.txt
+/home/ubuntu/.virtualenvs/alamo/bin/pip install -e /home/ubuntu/project/.
